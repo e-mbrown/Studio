580014314bc85c8847909e42466c3ccb91b0b583
e-mbrown <ebsonari@gmail.com>
2020-10-23 13:53:40 -0400

    Incorporated Jeremys changes to services

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -9,6 +9,9 @@ const Record = (props) => {
             <h2>{props.title}</h2>
             <h3>{props.artist} ||</h3>
             <p>{props.release}</p>
+            <button>
+                
+            </button>
         </div>
     )
 
--- src/services/index.js
+++ src/services/index.js
@@ -2,27 +2,46 @@ import Api from './apiConfig';
 import { apiUrl } from './apiConfig'
 import { changeHeader, clearHeader, changeMethod, altUrl} from './apiConfig';
 
-export const records = async () => {
-    try {
-        await changeMethod('get')
-        await altUrl('api/records')
-        await changeHeader()
-        const resp = await Api();
-        // const response = await resp.json()
-        return resp
-    } catch (error) {
-        throw error
+export const login = async (user) => {
+    const tempToken = localStorage.token
+    await delete localStorage.token
+    const resp = await Api.post('auth/users/login/', user)
+    if (resp) {
+        localStorage.token = resp.token
+    } else {
+        localStorage.token = tempToken
     }
+    return resp
 }
 
-export const login = async(user) => {
-    const resp = await fetch(apiUrl + 'auth/users/login/',{
-        method: 'POST',
-        headers: {
-            "Content-Type": 'Application/JSON'                
-        },
-        body: JSON.stringify(user)  
-    }
-).then(response => response.json())
-return resp
+export const records = async () => {
+    const resp = await Api.get('api/records/')
+    return resp
 }
+
+
+// Original code
+// export const records = async () => {
+//     try {
+//         await changeMethod('get')
+//         await altUrl('api/records')
+//         await changeHeader()
+//         const resp = await Api();
+//         // const response = await resp.json()
+//         return resp
+//     } catch (error) {
+//         throw error
+//     }
+// }
+
+// export const login = async(user) => {
+//     const resp = await fetch(apiUrl + 'auth/users/login/',{
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": 'Application/JSON'                
+//         },
+//         body: JSON.stringify(user)  
+//     }
+// ).then(response => response.json())
+// return resp
+// }
