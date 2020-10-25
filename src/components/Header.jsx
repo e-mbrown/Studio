c09d233027df2379c59908e2990a816146e7b2ac
weilyl <weily.lang@gmail.com>
2020-10-25 18:33:44 -0400

    changed navbar brand from default React-Bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ class Header extends React.Component {
         return(
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Records by Django</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
